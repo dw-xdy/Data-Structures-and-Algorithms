1f48772d2f420ff3573c7f208e6249d3a498f568
dw-xdy <3115708251@qq.com>
2026-01-14 11:53:17 +0800

    完成了对应的实现(暴力求解)

---
+++ LeetCode/Java/1486_xorOperation.java
@@ -0,0 +1,13 @@
+class Solution {
+    // 方法 1. (直接暴力) 
+    // 时间复杂度: O(n)
+    // 空间复杂度: O(1)
+    public static int xorOperation(int n, int start) {
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans ^= start + 2 * i;
+        }
+        return ans;
+    }
+}

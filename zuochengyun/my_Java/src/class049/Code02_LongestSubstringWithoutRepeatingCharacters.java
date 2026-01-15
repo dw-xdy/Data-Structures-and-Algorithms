48c7b8cd3081e0ad802e4da048cd59ce26e6134d
dw-xdy <3115708251@qq.com>
2026-01-15 23:11:51 +0800

    完成了左程云老师滑动窗口的第二道题目的Java版本

---
+++ zuochengyun/my_Java/src/class049/Code02_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,25 @@
+package class049;
+
+import java.util.Arrays;
+
+// 无重复字符的最长子串
+// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+// 测试链接 : https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+public class Code02_LongestSubstringWithoutRepeatingCharacters {
+
+	public static int lengthOfLongestSubstring(String str) {
+        char[] s = str.toCharArray();
+        int[] last = new int[256];
+        int ans = 0;
+        
+        Arrays.fill(last, -1);
+
+        for (int l = 0, r = 0; r < s.length; r++) {
+            ans = Math.max(ans, r - l + 1);
+            l = Math.max(l, last[s[r]] + 1);
+            last[s[r]] = r;
+        }
+        return ans;
+	}
+
+}

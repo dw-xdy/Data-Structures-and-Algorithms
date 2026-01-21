7fb21b094337bced93f2e02230da176cb95eb9af
dw-xdy <3115708251@qq.com>
2026-01-21 23:18:55 +0800

    完成了LeetCode第3429题的Java版本

---
+++ LeetCode/Java/Sliding Window/3439_maxFreeTime.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int n = startTime.length;
+        int[] free = new int[n + 1];
+        free[0] = startTime[0]; // 最左边的空余时间段
+        for (int i = 1; i < n; i++) {
+            free[i] = startTime[i] - endTime[i - 1]; // 中间的空余时间段
+        }
+        free[n] = eventTime - endTime[n - 1]; // 最右边的空余时间段
+
+        // 套定长滑窗模板（窗口长为 k+1）
+        int ans = 0;
+        int s = 0;
+        for (int i = 0; i <= n; i++) {
+            s += free[i];
+            if (i < k) {
+                continue;
+            }
+            ans = Math.max(ans, s);
+            s -= free[i - k];
+        }
+        return ans;
+    }
+}

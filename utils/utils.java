f9703fcebed322da50e098ebb6e4588f5634b4ab
dw-xdy <3115708251@qq.com>
2026-01-22 20:54:57 +0800

    为了弥补neovim的代码补全的导入库的问题, 增加一个utils.java用来导入库文件

---
+++ utils/utils.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Comparator;    // 自定义比较器
+import java.util.Arrays;        // 数组排序、二分查找等
+import java.util.Collections;   // 集合工具类
+import java.util.ArrayList;     // 动态数组
+import java.util.LinkedList;    // 链表
+import java.util.Queue;         // 队列接口
+import java.util.Deque;         // 双端队列
+import java.util.PriorityQueue; // 优先队列（堆）
+import java.util.Stack;         // 栈（不推荐，建议用Deque）
+import java.util.HashMap;       // 哈希表
+import java.util.HashSet;       // 哈希集合
+import java.util.TreeMap;       // 有序映射（红黑树）
+import java.util.TreeSet;       // 有序集合（红黑树）
